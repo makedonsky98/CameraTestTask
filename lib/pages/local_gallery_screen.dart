9e75a6cc1eafc142c2e22e1fd799e40e017b365d
Oleksandr Mytko <alex.mytko@gmail.com>
2025-11-26 18:26:59 +0200

    Added photo gallery screen

---
+++ lib/pages/local_gallery_screen.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class LocalGalleryScreen extends StatefulWidget {
+  const LocalGalleryScreen({super.key});
+
+  @override
+  State<LocalGalleryScreen> createState() => _LocalGalleryScreenState();
+}
+
+class _LocalGalleryScreenState extends State<LocalGalleryScreen> {
+  List<File> _images = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImages();
+  }
+
+  Future<void> _loadImages() async {
+    final appDir = await getApplicationDocumentsDirectory();
+    final List<FileSystemEntity> entities = appDir.listSync();
+
+    final imageFiles = entities
+        .whereType<File>()
+        .where((file) => file.path.endsWith('.jpg'))
+        .toList();
+
+    imageFiles.sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
+
+    setState(() {
+      _images = imageFiles;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Мої фото"),
+      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : _images.isEmpty
+          ? const Center(child: Text("Поки що немає фото"))
+          : GridView.builder(
+        padding: const EdgeInsets.all(8),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+        ),
+        itemCount: _images.length,
+        itemBuilder: (context, index) {
+          final file = _images[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) =>
+                  Scaffold(
+                    appBar: AppBar(),
+                    body: Center(child: Image.file(file)),
+                  )
+              ));
+            },
+            child: Image.file(
+              file,
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
